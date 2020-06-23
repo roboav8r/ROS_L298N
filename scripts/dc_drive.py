a99ab0499a8139994479b084f251ba003badfcc7
roboav8r <john.a.duncan@utexas.edu>
2020-06-23 15:52:53 -0500

    Under development. Initial pull from Jetson Nano.

---
+++ scripts/dc_drive.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys
+import time
+import RPi.GPIO as GPIO
+
+mode=GPIO.getmode()
+
+GPIO.cleanup()
+
+left_speed=32
+left_high= 
+
+right_speed=33
+sleeptime=1
+
+
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(Forward, GPIO.OUT)
+GPIO.setup(Backward, GPIO.OUT)
+
+def forward(x):
+	GPIO.output(Forward, GPIO.HIGH)
+	print("Moving Forward")
+	time.sleep(x)
+	GPIO.output(Forward, GPIO.LOW)
+
+def reverse(x):
+	GPIO.output(Backward, GPIO.HIGH)
+	print("Moving Backward")
+	time.sleep(x)
+	GPIO.output(Backward, GPIO.LOW)
+
+while (1):
+
+	forward(5)
+
+	reverse(5)
+	GPIO.cleanup()
